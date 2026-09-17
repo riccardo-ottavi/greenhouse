2b336539f986548d29e8f98c142cff41b48054b3
riccardo-ottavi <ottavi.ps@libero.it>
2026-09-17 15:13:06 +0200

    chore: init project structure

--- be/src/app.ts
+++ be/src/app.ts
@@ -1,8 +1,11 @@
-const express = require('express'); 
+import express from "express";
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("<h1>Greenhouse Monitoring System Homepage</h1>");
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
