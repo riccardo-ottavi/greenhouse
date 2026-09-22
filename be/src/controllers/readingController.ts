a194282d69c602b0cefb0832a8a6ad47efd5e553
riccardo-ottavi <ottavi.ps@libero.it>
2026-09-22 10:01:08 +0200

    feat: add readingController

---
+++ be/src/controllers/readingController.ts
@@ -0,0 +1,25 @@
+import { getAllReadings, getReadingById } from "../services/readingService";
+import { Request, Response } from "express";
+
+export function index(req: Request, res: Response) {
+    try {
+        const readings = getAllReadings();
+        res.json(readings);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Couldn't get readings" });
+    }
+}
+
+export function show(req: Request, res: Response) {
+    try {
+        const id = Number(req.params.id);
+        const reading = getReadingById(id);
+        res.json(reading);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message: "Couldn't get reading's data"})
+    }
+}
